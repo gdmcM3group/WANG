db6cd148a76399061b735777881384353cd5d40b
Wang Ruqin <miaomiao3141@yahoo.com>
2020-11-29 20:14:27 +0800

    test:GDMC filters

---
+++ test/README.md
@@ -0,0 +1,4 @@
+# ICE_JIT
+Japan Immersion Travel! for GDMC2020
+
+ Please see the outline of our generator [here]( https://www.dropbox.com/s/apd6utnngm6o3dl/ICE_JIT.pdf?dl=0).
---
+++ test/functions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+def getBoxSize(box):
+    return box.maxx - box.minx, box.maxy - box.miny, box.maxz - box.minz
+
+# input:box
+# output:boxsize
+
+def get_block(level, x, y, z):
+    block_id = level.blockAt(x, y, z)
+    data = level.blockDataAt(x, y, z)
+    return block_id, data
+
+
+def setBlock(level, x, y, z, block_id, data=0):
+    level.setBlockAt(x, y, z, block_id)
+    level.setBlockDataAt(x, y, z, data)
+
+    # def setBlock
+    # input:level,x,y,z,block_id(int)
+    # creat a new block
+    
